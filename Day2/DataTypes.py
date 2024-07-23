c2c95287667490f094154b1b947dc59411b15aab
Michael Selvan <michael.selvan2005@gmail.com>
2024-07-23 10:40:37 +0530

    DataTypes

---
+++ Day2/DataTypes.py
@@ -0,0 +1,45 @@
+print(len("Hello"))
+#print(len(1234)) # Error
+
+#print("Hello"[9])
+
+
+#string
+
+print("123" +"456")
+
+#Integer
+
+print(123+456)
+
+# Float
+
+3.14
+
+# Boolean
+
+True
+False
+
+street_name="Abbey Road"
+print(street_name[4]+street_name[7])
+
+num_char=len(input("What is your name?"))
+
+print(f"testing {num_char}")
+new_num_char=str(num_char)
+
+print("Your name is"+new_num_char+"Character")
+
+
+#print("hello"+1+"selvan") // TypeError: can only concatenate str (not "int") to str
+
+# PEMDAS
+# Paranthesis ()
+# Exponents      **
+# Multiplication *
+# Division       /
+# Addition       +
+# Substraction   - 
+
+print(3*3+3/3-3)
